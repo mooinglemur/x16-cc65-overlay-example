efe9ebd5de8b19408003ca8bd776d9cc6e2b3f00
mooinglemur <mooinglemur@users.noreply.github.com>
2025-11-20 23:03:24 -0700

    adjust comments

--- src/main.c
+++ src/main.c
@@ -8,9 +8,12 @@ void __fastcall__ print_message(const char *msg) {
 	cprintf("%s", msg);
 }
 
-// Main entry point has to be in C because
-// C does some initialization for its own state
-// that we can't reach if we were to start in ASM
+// Main entry point is in C because
+// we need C to initialize its own state
+// before we can call C functions.
+
+// But we jump right into our ASM code
+// afterwards. :)
 
 void main(void) {
 	start(); // calls assembly routine
