3341194a4e9efa406c49ad0559b0e3c7dc12f170
mooinglemur <mooinglemur@users.noreply.github.com>
2025-11-20 23:54:36 -0700

    make whitespace consistent

--- src/overlay1.c
+++ src/overlay1.c
@@ -5,6 +5,6 @@
 #pragma data-name ("OVERLAY1DATA")
 
 void __fastcall__ overlay1func(short count) {
-    cprintf("I am in OVERLAY1, my function address is 0x%04X, and my parameter is %d\r\n", &overlay1func, count);
+	cprintf("I am in OVERLAY1, my function address is 0x%04X, and my parameter is %d\r\n", &overlay1func, count);
 }
 
